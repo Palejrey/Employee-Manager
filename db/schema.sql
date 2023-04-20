4c63393c13b37ac5bd6b957c8b8e9b9add08ade4
palejrey@gmail.com <palejrey@gmail.com>
2023-04-19 23:16:48 -0500

    Updated seeds.sql along with updated server.js

--- db/schema.sql
+++ db/schema.sql
@@ -4,24 +4,27 @@ CREATE DATABASE department_db;
 USE department_db;
 
 CREATE TABLE department(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT ,
     name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department (name)
+    salary DECIMAL NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
+    role_id INT NOT NULL,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role (title)
+    PRIMARY KEY(id),
+    FOREIGN KEY(role_id) REFERENCES role(id)
 );
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
 const PORT = process.env.port || 3001;
 const app = express();
 
@@ -33,7 +34,48 @@ inquirer
         {
             type: 'list',
             message: 'What would you like to do ?',
-            name: 'choices',
-            choices: ["Update Employee Role","View All Roles","Add Role","View All Departments","Quit","View All Employees"],
+            name: 'options',
+            choices: ["View All Departments","View All Roles","View all Employee","Add a Department","Add a Role","Add an Employee","Update an Employee Role","Quit"],
         }
-    ]);
+    ])
+    .then((response) =>{
+        switch(response.options){
+            case 'View All Departments':
+                db.query('SELECT * FROM department_db.department', function (err, results) {
+                    console.table(results);
+                });
+            break;
+
+            case'View All Roles':
+                db.query('SELECT * FROM department_db.role', function (err, results) {
+                    console.table(results);
+                 });
+            break;
+
+            case'View all Employee':
+            db.query('SELECT * FROM department_db.employee', function (err, results) {
+                console.table(results);
+            });
+            break;
+
+            case'Add a Department':
+                console.log("WIP");
+            break;
+
+            case'Add a Role':
+                console.log("WIP");
+            break;
+
+            case'Add an Employee':
+                console.log("WIP");
+            break;
+
+            case'Update an Employee Role':
+                console.log("WIP");
+            break;
+
+            case'Quit':
+                console.log("WIP");
+                break;
+        }
+    });
